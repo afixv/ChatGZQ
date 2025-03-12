9f6cab59a409d8d44c742c7df0e29177689a83df
Anantadhirya <anantadhirya@gmail.com>
2025-03-12 22:10:38 +0700

    feat: pages placeholder

---
+++ app/(pages)/dashboard/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <main className="flex h-screen flex-col items-center justify-center">
+      <h1 className="font-semibold">Dashboard</h1>
+      <span className="text-sm">Lorem ipsum</span>
+    </main>
+  );
+}
